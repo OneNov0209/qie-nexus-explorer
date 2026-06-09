be47fba161fc35c4a567efe6fee5e74a82afa2ab
OneNov0209 <95606017+OneNov0209@users.noreply.github.com>
2026-06-09 14:38:28 +0800

    Update network.ts

--- src/data/network.ts
+++ src/data/network.ts
@@ -1,7 +1,7 @@
 export const NETWORK = {
   name: "QIE Mainnet",
   chainId: 1990,
-  chainIdHex: "0x7C6", // 1990
+  chainIdHex: "0x7C6",
   cosmosChainId: "qie_1990-1",
   rpc: "https://rpc.qie.onenov.xyz",
   rest: "https://api.qie.onenov.xyz",
@@ -25,21 +25,43 @@ export const WALLET_LOGOS = {
 export const FOOTER_LINKS = {
   products: [
     { label: "QIE Blockchain", href: "https://www.qie.digital/" },
-    { label: "QIE Wallet", href: "https://www.qiewallet.me/" },
+    { label: "QIE Wallet", href: "https://qiewallet.me/" },
+    { label: "QIE Pass", href: "https://qiepass.qie.digital/" },
+    { label: "QIE Domains", href: "https://domains.qie.digital/" },
+    { label: "QIE DEX", href: "https://www.dex.qie.digital/#/stake" },
+    { label: "QIE Lottery", href: "https://lottery.qie.digital/" },
+    { label: "QIE Doodle", href: "https://qiedoodle.com/" },
+    { label: "QIE Lend", href: "https://www.qielend.qie.digital/" },
   ],
   developers: [
     { label: "Documentation", href: "https://docs.qie.digital/" },
+    { label: "Developer Portal", href: "https://www.qie.digital/developer#" },
     { label: "GitHub", href: "https://github.com/qieadmin" },
+    { label: "IBC Explorer", href: "/ibc" },
+    { label: "Chain Parameters", href: "/parameters" },
+  ],
+  ecosystem: [
+    { label: "QIE Bridge", href: "https://bridge.qie.digital/" },
+    { label: "QBots Trade", href: "https://www.qbots.trade/" },
+    { label: "Pawsome Host", href: "https://www.pawsome.host/" },
   ],
   community: [
-    { label: "Discord", href: "https://discord.com/invite/8DD4kSHBvr" },
+    { label: "X (Twitter)", href: "https://x.com/qieblockchain" },
     { label: "Telegram", href: "https://t.me/HovRonQiblockchain" },
-    { label: "Reddit", href: "https://reddit.com/r/QIEBlockchain" },
-    { label: "Blog", href: "https://www.qiewallet.me/blogs" },
+    { label: "Discord", href: "https://discord.com/invite/8DD4kSHBvr" },
+    { label: "Facebook", href: "https://www.facebook.com/QiBlockchain" },
+    { label: "Instagram", href: "https://www.instagram.com/qieblockchain.online/" },
+    { label: "Reddit", href: "https://www.reddit.com/r/qiblockchain/?rdt=58368" },
   ],
-  hackathon: [
-    { label: "Prizes", href: "https://hackathon.qie.digital/#prizes" },
-    { label: "FAQ", href: "https://hackathon.qie.digital/#faq" },
+  company: [
+    { label: "About", href: "https://www.qie.digital/developer#" },
+    { label: "Blog", href: "https://www.qie.digital/blog" },
+    { label: "Medium", href: "https://medium.com/@QIEecosystem/" },
+    { label: "Contact", href: "mailto:info@qiewallet.me" },
   ],
-  email: "Info@qie.digital",
-};
+  legal: [
+    { label: "Terms of Service", href: "https://www.qie.digital/terms-of-services" },
+    { label: "Privacy Policy", href: "https://www.qie.digital/terms-of-services" },
+  ],
+  email: "info@qiewallet.me",
+} as const;
